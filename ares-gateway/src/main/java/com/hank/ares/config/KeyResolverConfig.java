1af811a4b41cc2772d556454b7df1a715c739529
shihuan <shihuan@wayyue.com>
2021-04-08 16:25:53 +0800

    feat(ares-gateway):add param request rate limiter

--- ares-gateway/src/main/java/com/hank/ares/config/KeyResolverConfig.java
+++ ares-gateway/src/main/java/com/hank/ares/config/KeyResolverConfig.java
@@ -1,20 +1,33 @@
 package com.hank.ares.config;
 
+import com.hank.ares.enums.ReqParamEnum;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class KeyResolverConfig {
 
     /**
-     * 限流规则
+     * URI限流
      *
      * @return
      */
     @Bean
     public KeyResolver pathKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getURI().getPath());
+    }
+
+    /**
+     * 参数限流
+     *
+     * @return
+     */
+    @Bean
+    @Primary
+    public KeyResolver parameterKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst(ReqParamEnum.ARES_USER_ID.getParamKey()));
     }
 }
