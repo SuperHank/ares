efb7335ad52abff17a4cb3c3cc1a24a547d9f4bc
shihuan <shihuan@wayyue.com>
2021-04-08 11:35:23 +0800

    feat(ares-gateway):add global filter

---
+++ ares-gateway/src/main/java/com/hank/ares/filter/AuthenticateGlobalFilter.java
@@ -0,0 +1,69 @@
+package com.hank.ares.filter;
+
+import com.hank.ares.enums.ReqParamEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局认证过滤器
+ */
+@Slf4j
+@Component
+public class AuthenticateGlobalFilter implements GlobalFilter, Ordered {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String userId = exchange.getRequest().getQueryParams().getFirst(ReqParamEnum.ARES_USER_ID.getParamKey());
+        String userToken = exchange.getRequest().getQueryParams().getFirst(ReqParamEnum.ARES_TOKEN.getParamKey());
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(userToken)) {
+            log.error("userId or userToken is null. userId:{},userToken:{}", userId, userToken);
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            String message = String.format("message:%s", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            DataBuffer buffer = response.bufferFactory().wrap(message.getBytes());
+            return response.writeWith(Mono.just(buffer));
+        }
+
+        String tokenInCache = redisTemplate.opsForValue().get(userId);
+
+        if (StringUtils.isBlank(tokenInCache)) {
+            log.error("token in cache is null...");
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            String message = String.format("message:%s", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            DataBuffer buffer = response.bufferFactory().wrap(message.getBytes());
+            return response.writeWith(Mono.just(buffer));
+        }
+
+        if (!StringUtils.equals(tokenInCache, userToken)) {
+            log.error("token in wrong...");
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            String message = String.format("message:%s", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            DataBuffer buffer = response.bufferFactory().wrap(message.getBytes());
+            return response.writeWith(Mono.just(buffer));
+        }
+
+        log.info("token is ok!");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
---
+++ ares-gateway/src/main/java/com/hank/ares/filter/CustomGatewayFilter.java
@@ -0,0 +1,34 @@
+package com.hank.ares.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class CustomGatewayFilter implements GatewayFilter, Ordered {
+    /**
+     * 过滤器业务逻辑
+     *
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("自定义网关过滤器被执行");
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 过滤器执行顺序，数值越小，优先级越高
+     *
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
