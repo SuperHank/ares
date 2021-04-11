b68b9070a1bb9d277b92c35598317b42be4e9689
shih <18852854653@163.com>
2021-04-11 22:18:32 +0800

    feat(ares-order):add ribbon

--- ares-order/src/main/java/com/hank/ares/controller/OrderController.java
+++ ares-order/src/main/java/com/hank/ares/controller/OrderController.java
@@ -1,9 +1,11 @@
 package com.hank.ares.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/order")
@@ -11,6 +13,10 @@ public class OrderController {
 
     @Value("${spring.profiles.active}")
     private String currentEnvironment;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("hello")
     private String hello() {
@@ -20,5 +26,10 @@ public class OrderController {
     @GetMapping("index")
     private String index() {
         return String.format("order-%s index", currentEnvironment);
+    }
+
+    @GetMapping("settlementHello")
+    private String settlementhello(){
+        return restTemplate.getForObject("http://ARES-SETTLEMENT/settlement/settlement/hello",String.class);
     }
 }
