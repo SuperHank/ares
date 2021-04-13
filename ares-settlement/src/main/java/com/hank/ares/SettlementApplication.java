0aaa0debd2164bf155d14b591bab9c247016731e
shihuan <shihuan@wayyue.com>
2021-04-13 19:47:59 +0800

    feat(ares-settlement):add quartz remove quartz model

--- ares-settlement/src/main/java/com/hank/ares/SettlementApplication.java
+++ ares-settlement/src/main/java/com/hank/ares/SettlementApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("com.hank.ares.mapper")
+@EnableScheduling
 public class SettlementApplication {
 
     public static void main(String[] args) {
